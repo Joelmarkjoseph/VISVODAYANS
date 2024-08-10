4d7a4974663b24447e0f9880cdc4f694a1331124
Joel Mark Joseph S <joelmarkjoseph2004@gmail.com>
2024-08-10 21:55:03 +0530

    New Commits

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const LANGUAGE_VERSIONS = {
+  javascript: "18.15.0",
+  typescript: "5.0.3",
+  python: "3.10.0",
+  java: "15.0.2",
+  csharp: "6.12.0",
+  php: "8.2.3",
+};
+
+export const CODE_SNIPPETS = {
+  javascript: `\nconsole.log("Hello Tally Codebreweeeessss!" );\n`,
+  typescript: `\nfunction greet() {\n\tconsole.log("Hello Tally Codebreweeeessss!");\n}\n\ngreet();\n`,
+  python: `\nprint("Hello Tally Codebreweeeessss!")\n`,
+  java: `\npublic class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello Tally Codebreweeeessss!");\n\t}\n}\n`,
+  csharp:
+    'using System;\n\nnamespace HelloWorld\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Hello Tally Codebreweeeessss!");\n\t\t}\n\t}\n}\n',
+  php: "<?php\n\n$msg = 'Hello Tally Codebreweeeessss!';\necho $msg;\n",
+};
